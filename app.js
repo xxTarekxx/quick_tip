2cd576d2f12a72a3bef7b5a6d4b7efc88496c75d
Tarek <52683281+xxTarekxx@users.noreply.github.com>
2021-08-26 15:39:41 -0500

    adding calculator function

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
+function currency(value){
+   value = value.toFixed(2);
+   return "$ " + value;
+}
+
 function update(){
    let bill = Number(document.getElementById("yourBill").value);
    let tipPercent = document.getElementById("tipInput").value;
    let split = document.getElementById("splitInput").value;
+   
+
 
    let tipValue = bill * (tipPercent / 100);
    let tipEach = tipValue / split;
    let newBillEach = (bill + tipValue) / split;
-
+   let billEach = bill / split;
+   
    document.getElementById('tipPercent').innerHTML = "%" + tipPercent;
-   document.getElementById('tipValue').innerHTML = "$" + tipValue.toFixed(2);
+   document.getElementById('tipValue').innerHTML = currency(tipValue);
+   document.getElementById('totalWithTip').innerHTML = currency(bill + tipValue);
+   document.getElementById('splitValue').innerHTML = splitInput.value + " Person(s)";
+   document.getElementById('billEach').innerHTML = currency(billEach);
+   document.getElementById('tipEach').innerHTML = currency(tipEach);
 }
 
 let container = document.getElementById('container');
